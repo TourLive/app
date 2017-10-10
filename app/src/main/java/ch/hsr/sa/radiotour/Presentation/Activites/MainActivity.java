a068205703b6f42ffc6a7ad15af7d1e2c87d8f87
Urs Forrer <mail@ursforrer.ch>
2017-10-10 19:55:28 +0200

    RADIOTOUR-73 Added main navigation to the app

--- app/src/main/java/ch/hsr/sa/radiotour/Presentation/Activites/MainActivity.java
+++ app/src/main/java/ch/hsr/sa/radiotour/Presentation/Activites/MainActivity.java
@@ -10,25 +10,25 @@ import ch.hsr.sa.radiotour.Controller.Adapter.ViewPageAdapter;
 import ch.hsr.sa.radiotour.Presentation.fragments.ImportFragment;
 import ch.hsr.sa.radiotour.Presentation.fragments.MaillotsFragment;
 import ch.hsr.sa.radiotour.Presentation.fragments.RaceFragment;
+import ch.hsr.sa.radiotour.Presentation.fragments.SpecialFragment;
+import ch.hsr.sa.radiotour.Presentation.fragments.VirtualClassFragment;
 import ch.hsr.sa.radiotour.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.v("TAG", "TEST");
         super.onCreate(savedInstanceState);
-        Log.v("TAG", "TEST");
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         ViewPageAdapter viewPageAdapter = new ViewPageAdapter(getSupportFragmentManager());
 
         viewPageAdapter.addFragment(new RaceFragment(), getString(R.string.header_race));
+        viewPageAdapter.addFragment(new SpecialFragment(), getString(R.string.header_special_class));
+        viewPageAdapter.addFragment(new VirtualClassFragment(), getString(R.string.header_virtual_class));
         viewPageAdapter.addFragment(new MaillotsFragment(), getString(R.string.header_maillots));
         viewPageAdapter.addFragment(new ImportFragment(), getString(R.string.header_import_data));
-
-        Log.d("TAG", "TEST");
 
         viewPager.setAdapter(viewPageAdapter);
 
---
+++ app/src/main/java/ch/hsr/sa/radiotour/Presentation/fragments/VirtualClassFragment.java
@@ -0,0 +1,23 @@
+package ch.hsr.sa.radiotour.Presentation.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ch.hsr.sa.radiotour.R;
+
+/**
+ * Created by Urs Forrer on 10.10.2017.
+ */
+
+public class VirtualClassFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("TAG","VirtualClassFragment onCreateView");
+        View root = inflater.inflate(R.layout.fragment_virtualclass, container, false);
+        return root;
+    }
+}
