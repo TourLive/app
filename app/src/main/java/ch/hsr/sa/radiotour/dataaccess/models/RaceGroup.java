cb48df7905581a28f71c0711eab074be8cf0d789
Urs Forrer <mail@ursforrer.ch>
2017-10-13 19:37:49 +0200

    Initial dataaccess files
    Initial dataaccess files

---
+++ app/src/main/java/ch/hsr/sa/radiotour/dataaccess/models/RaceGroup.java
@@ -0,0 +1,8 @@
+package ch.hsr.sa.radiotour.dataaccess.models;
+
+/**
+ * Created by Urs Forrer on 13.10.2017.
+ */
+
+public class RaceGroup {
+}
---
+++ app/src/main/java/ch/hsr/sa/radiotour/dataaccess/repositories/RaceGroupRepository.java
@@ -0,0 +1,8 @@
+package ch.hsr.sa.radiotour.dataaccess.repositories;
+
+/**
+ * Created by Urs Forrer on 13.10.2017.
+ */
+
+public class RaceGroupRepository {
+}
