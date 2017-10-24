19e49d6de55d467ca5668eccfda7809221d8cec3
Hawner <dominik_good@hotmail.com>
2017-10-24 14:29:40 +0200

    Rider Tests Completed

--- app/src/androidTest/java/ch/hsr/sa/radiotour/RiderRepositoryInstrumentedTest.java
+++ app/src/androidTest/java/ch/hsr/sa/radiotour/RiderRepositoryInstrumentedTest.java
@@ -1,25 +1,22 @@
 package ch.hsr.sa.radiotour;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
-import com.facebook.stetho.Stetho;
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
+import java.util.Date;
 
 import ch.hsr.sa.radiotour.dataaccess.RadioTourApplication;
-import ch.hsr.sa.radiotour.dataaccess.RealmModul;
 import ch.hsr.sa.radiotour.dataaccess.interfaces.IRiderRepository;
+import ch.hsr.sa.radiotour.dataaccess.interfaces.IRiderStageConnectionRepository;
 import ch.hsr.sa.radiotour.dataaccess.models.Rider;
+import ch.hsr.sa.radiotour.dataaccess.models.RiderStageConnection;
+import ch.hsr.sa.radiotour.dataaccess.models.RiderStateType;
 import ch.hsr.sa.radiotour.dataaccess.repositories.RiderRepository;
+import ch.hsr.sa.radiotour.dataaccess.repositories.RiderStageConnectionRepository;
 import io.realm.Realm;
-import io.realm.RealmConfiguration;
 import io.realm.RealmList;
 import io.realm.RealmResults;
 
@@ -30,15 +27,19 @@ import static org.junit.Assert.assertEquals;
 public class RiderRepositoryInstrumentedTest {
 
     private Realm realm;
-    private RiderRepository repository;
+    private RiderRepository riderRepository;
+    private RiderStageConnectionRepository riderStageConnectionRepository;
     private IRiderRepository.OnSaveRiderCallback onSaveRiderCallback;
     private IRiderRepository.OnGetAllRidersCallback onGetAllRidersCallback;
+    private IRiderRepository.OnUpdateRiderStageCallback OnUpdateRiderStageCallback;
+    private IRiderStageConnectionRepository.OnSaveRiderStageConnectionCallback onSaveRiderStageConnectionCallbackCallback;
 
     private RealmList<Rider> riders = new RealmList<>();
 
     @Before
     public void initTestData() {
-        this.repository = new RiderRepository();
+        this.riderRepository = new RiderRepository();
+        this.riderStageConnectionRepository = new RiderStageConnectionRepository();
         realm = Realm.getInstance(RadioTourApplication.getInstance());
         initCallbacks();
         riders.clear();
@@ -46,6 +47,7 @@ public class RiderRepositoryInstrumentedTest {
             @Override
             public void execute(Realm realm) {
                 realm.where(Rider.class).findAll().deleteAllFromRealm();
+                realm.where(RiderStageConnection.class).findAll().deleteAllFromRealm();
             }
         });
     }
@@ -73,6 +75,29 @@ public class RiderRepositoryInstrumentedTest {
             public void onError(String message) {
             }
         };
+
+        OnUpdateRiderStageCallback = new IRiderRepository.OnUpdateRiderStageCallback() {
+            @Override
+            public void onSuccess() {
+                updateRiderStateSuccessfully();
+            }
+
+            @Override
+            public void onError(String message) {
+            }
+        };
+
+        onSaveRiderStageConnectionCallbackCallback = new IRiderStageConnectionRepository.OnSaveRiderStageConnectionCallback() {
+            @Override
+            public void onSuccess() {
+                saveRiderStageConnectionSuccessfully();
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+        };
     }
 
     @Test
@@ -82,7 +107,7 @@ public class RiderRepositoryInstrumentedTest {
         rider.setName("testrider");
         rider.setStartNr(15);
         synchronized (this){
-            repository.addRider(rider, onSaveRiderCallback);
+            riderRepository.addRider(rider, onSaveRiderCallback);
         }
         RealmResults<Rider> riders = realm.where(Rider.class).findAll();
         RealmList<Rider> res = new RealmList<>();
@@ -103,11 +128,11 @@ public class RiderRepositoryInstrumentedTest {
             rider.setName("testrider" + 1);
             rider.setStartNr(i);
             synchronized (this){
-                repository.addRider(rider, onSaveRiderCallback);
+                riderRepository.addRider(rider, onSaveRiderCallback);
             }
         }
         synchronized (this){
-            repository.getAllRiders(onGetAllRidersCallback);
+            riderRepository.getAllRiders(onGetAllRidersCallback);
         }
         assertEquals(3, riders.size());
         assertEquals(2, riders.get(1).getStartNr());
@@ -121,18 +146,57 @@ public class RiderRepositoryInstrumentedTest {
             rider.setName("testrider" + 1);
             rider.setStartNr(i);
             synchronized (this){
-                repository.addRider(rider, onSaveRiderCallback);
+                riderRepository.addRider(rider, onSaveRiderCallback);
             }
         }
-        riders = repository.getAllRidersReturned();
+        riders = riderRepository.getAllRidersReturned();
         assertEquals(3, riders.size());
         assertEquals(2, riders.get(1).getStartNr());
     }
 
     @Test
     public void clearAllRiders(){
-        repository.clearAllRiders();
-        assertEquals(0, repository.getAllRidersReturned().size());
+        riderRepository.clearAllRiders();
+        assertEquals(0, riderRepository.getAllRidersReturned().size());
+    }
+
+    @Test
+    public void updateRiderStateConnection(){
+
+        RiderStageConnection riderStageConnection = new RiderStageConnection();
+        riderStageConnection.setRank(1);
+        riderStageConnection.setOfficialTime(new Date(100));
+        riderStageConnection.setOfficialGap(new Date(100));
+        riderStageConnection.setVirtualGap(new Date(100));
+        riderStageConnection.setBonusPoint(100);
+        riderStageConnection.setBonusTime(200);
+        riderStageConnection.setType(RiderStateType.AKTIVE);
+        synchronized (this){
+            riderStageConnectionRepository.addRiderStageConnection(riderStageConnection, onSaveRiderStageConnectionCallbackCallback);
+        }
+
+        Rider rider = new Rider();
+        rider.setCountry("swiss");
+        rider.setName("testrider" + 1);
+        rider.setStartNr(15);
+
+        synchronized (this){
+            riderRepository.addRider(rider, onSaveRiderCallback);
+        }
+
+
+        RealmList<RiderStageConnection> connections = new RealmList<>();
+        synchronized (this){
+            connections.add(riderStageConnectionRepository.getRiderByRank(1));
+            riderRepository.updateRiderStageConnection(rider, connections, OnUpdateRiderStageCallback);
+        }
+
+        Rider realmRider = realm.where(Rider.class).equalTo("startNr", rider.getStartNr()).findFirst();
+        RealmList<RiderStageConnection> riderStageConnections = realmRider.getRiderStages();
+
+        assertEquals(1, riderStageConnections.get(0).getRank());
+        assertEquals(100, riderStageConnections.get(0).getBonusPoint());
+        assertEquals(200, riderStageConnections.get(0).getBonusTime());
     }
 
     private void riderAddedSuccessfully(){
@@ -141,6 +205,14 @@ public class RiderRepositoryInstrumentedTest {
 
     private void getAllRidersSuccessfully(RealmList<Rider> riders){
         this.riders.addAll(riders);
+        notifyAll();
+    }
+
+    private void updateRiderStateSuccessfully(){
+        notifyAll();
+    }
+
+    private void saveRiderStageConnectionSuccessfully() {
         notifyAll();
     }
 }
