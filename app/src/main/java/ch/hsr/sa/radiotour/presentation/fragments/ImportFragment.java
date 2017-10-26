8ef65c7af091c1da1f48797f12cf0086b59782c6
Hawner <dominik_good@hotmail.com>
2017-10-26 09:18:27 +0200

    Button Import

--- app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/ImportFragment.java
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/ImportFragment.java
@@ -6,14 +6,31 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import ch.hsr.sa.radiotour.R;
+import ch.hsr.sa.radiotour.controller.api.APIClient;
+import ch.hsr.sa.radiotour.controller.api.UrlLink;
 
-public class ImportFragment extends Fragment {
+public class ImportFragment extends Fragment implements View.OnClickListener  {
+    private Button btn_Import;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d("TAG","ImportFragment onCreateView");
         View root = inflater.inflate(R.layout.fragment_import, container, false);
+        btn_Import = (Button) root.findViewById(R.id.btn_Import);
+        btn_Import.setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btn_Import){
+            try{
+                APIClient.getRiders(UrlLink.RIDERS, null);
+            } catch (Exception ex){
+
+            }
+        }
     }
 }
