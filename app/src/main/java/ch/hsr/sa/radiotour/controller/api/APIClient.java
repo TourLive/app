e096c827bb66a72348888fe317789808e0e59a34
Hawner <dominik_good@hotmail.com>
2017-10-26 08:55:48 +0200

    Setup for Http Client

--- app/src/main/java/ch/hsr/sa/radiotour/controller/api/APIClient.java
+++ app/src/main/java/ch/hsr/sa/radiotour/controller/api/APIClient.java
@@ -1,4 +1,55 @@
 package ch.hsr.sa.radiotour.controller.api;
 
-public class APIClient {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public final class APIClient {
+        private static final String BASE_URL = "https://tlng.cnlab.ch/";
+
+        private static AsyncHttpClient client = new AsyncHttpClient();
+
+        private static void get(UrlLink url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.get(getAbsoluteUrl(url.toString()), params, responseHandler);
+        }
+
+        private static void post(UrlLink url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.post(getAbsoluteUrl(url.toString()), params, responseHandler);
+        }
+
+        private static String getAbsoluteUrl(String relativeUrl) {
+            return BASE_URL + relativeUrl;
+        }
+
+        public static void getRiders(UrlLink url, RequestParams params, AsyncHttpResponseHandler responseHandler) throws JSONException{
+            APIClient.get(url, null, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    // If the response is JSONObject instead of expected JSONArray
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray riders) {
+                    // Pull out the first event on the public timeline
+                    try{
+                        JSONObject firstEvent = riders.getJSONObject(0);
+                        String tweetText = firstEvent.getString("name");
+                        System.out.println(tweetText);
+                    } catch (Exception ex){
+
+                    }
+
+                    // Do something with the response
+
+                }
+            });
+
+        }
 }
---
+++ app/src/main/java/ch/hsr/sa/radiotour/controller/api/UrlLink.java
@@ -0,0 +1,20 @@
+package ch.hsr.sa.radiotour.controller.api;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public final class UrlLink {
+    public static final List<String> RIDERS =
+            Arrays.asList("api/masterdata/riders/stage/198", "api/masterdata/riders/stage/199",
+                    "api/masterdata/riders/stage/200", "api/masterdata/riders/stage/201",
+                    "api/masterdata/riders/stage/202", "api/masterdata/riders/stage/203",
+                    "api/masterdata/riders/stage/204", "api/masterdata/riders/stage/205",
+                    "api/masterdata/riders/stage/206");
+
+    public static final String STAGES = "public/stages/109";
+
+    public static final String JUDGEMENTS = "api/masterdata/judgements/109";
+
+    public static final String STATES = "json_public/status.php";
+}
