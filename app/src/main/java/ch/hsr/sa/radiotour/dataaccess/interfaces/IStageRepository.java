90e75c96af77fa3a151cbb742526a4ab24218c06
Hawner <dominik_good@hotmail.com>
2017-10-30 13:10:52 +0100

    Presenter & Repository for Stages implemented

---
+++ app/src/main/java/ch/hsr/sa/radiotour/dataaccess/interfaces/IStageRepository.java
@@ -0,0 +1,15 @@
+package ch.hsr.sa.radiotour.dataaccess.interfaces;
+
+import ch.hsr.sa.radiotour.dataaccess.models.Stage;
+
+public interface IStageRepository {
+
+    interface OnSaveStageCallback {
+        void onSuccess();
+        void onError(String message);
+    }
+
+    void addStage(Stage stage, OnSaveStageCallback callback);
+
+    void clearAllStages();
+}
