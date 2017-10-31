932de3659950bd5a1253bf9df8c23055da3737db
Hawner <dominik_good@hotmail.com>
2017-10-31 12:05:39 +0100

    Implementation of ProgressBar

--- app/src/main/java/ch/hsr/sa/radiotour/controller/api/APIClient.java
+++ app/src/main/java/ch/hsr/sa/radiotour/controller/api/APIClient.java
@@ -2,18 +2,15 @@ package ch.hsr.sa.radiotour.controller.api;
 
 import android.util.Log;
 
-import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URL;
-
-import ch.hsr.sa.radiotour.business.Context;
 import ch.hsr.sa.radiotour.business.Parser;
 import cz.msebera.android.httpclient.Header;
 
@@ -22,13 +19,15 @@ public final class APIClient {
     private static String RACE_ID = "";
     private static String STAGE_ID = "";
 
-    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static SyncHttpClient client = new SyncHttpClient();
 
     private static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        responseHandler.setUseSynchronousMode(true);
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     private static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        responseHandler.setUseSynchronousMode(true);
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
@@ -36,24 +35,25 @@ public final class APIClient {
         return BASE_URL + relativeUrl;
     }
 
-    public static void importData() throws JSONException{
+    public static void deleteData() throws JSONException{
         clearDatabase();
         getActualRaceId(UrlLink.GLOBALSETTINGS, null);
     }
 
-    public static void fillData() throws JSONException{
-        synchronized (APIClient.client) {
-            getRiders(UrlLink.RIDERS + STAGE_ID, null);
-        }
-        synchronized (APIClient.client) {
-            getJudgments(UrlLink.JUDGEMENTS + RACE_ID, null);
-        }
-        synchronized (APIClient.client){
-            getRewards(UrlLink.JUDGEMENTS + RACE_ID, null);
-        }
-        synchronized (APIClient.client){
-            getStages(UrlLink.STAGES + RACE_ID, null);
-        }
+    public static void getRiders() throws JSONException{
+        getRiders(UrlLink.RIDERS + STAGE_ID, null);
+    }
+
+    public static void getJudgments() throws JSONException{
+        getJudgments(UrlLink.JUDGEMENTS + RACE_ID, null);
+    }
+
+    public static void getRewards() throws JSONException{
+        getRewards(UrlLink.JUDGEMENTS + RACE_ID, null);
+    }
+
+    public static void getStages() throws JSONException{
+        getStages(UrlLink.STAGES + RACE_ID, null);
     }
 
     public static void clearDatabase(){
@@ -72,7 +72,6 @@ public final class APIClient {
                 try{
                     STAGE_ID = settings.getJSONObject(0).getString("parameter");
                     RACE_ID = settings.getJSONObject(1).getString("parameter");
-                    fillData();
                 } catch (JSONException ex){
                     Log.d("error", ex.getMessage());
                 }
--- app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/ImportFragment.java
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/ImportFragment.java
@@ -1,12 +1,18 @@
 package ch.hsr.sa.radiotour.presentation.fragments;
 
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
+
+import org.json.JSONException;
 
 import ch.hsr.sa.radiotour.R;
 import ch.hsr.sa.radiotour.controller.api.APIClient;
@@ -14,12 +20,17 @@ import ch.hsr.sa.radiotour.controller.api.UrlLink;
 
 public class ImportFragment extends Fragment implements View.OnClickListener  {
     private Button btn_Import;
+    private ProgressDialog progressBar;
+    private int progressBarStatus;
+    private Handler progressBarHandler = new Handler();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d("TAG","ImportFragment onCreateView");
         View root = inflater.inflate(R.layout.fragment_import, container, false);
         btn_Import = (Button) root.findViewById(R.id.btn_Import);
         btn_Import.setOnClickListener(this);
+        progressBar = new ProgressDialog(getActivity());
         return root;
     }
 
@@ -27,10 +38,86 @@ public class ImportFragment extends Fragment implements View.OnClickListener  {
     public void onClick(View v) {
         if(v == btn_Import){
             try{
-                APIClient.importData();
+                progressBar.setCancelable(true);
+                progressBar.setMessage("Import starting ...");
+                progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressBar.setProgress(0);
+                progressBar.setMax(100);
+                progressBar.show();
+                progressBarStatus = 0;
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressBarStatus < 100) {
+                            try{
+                                progressBarStatus = importData();
+                            } catch (JSONException ex){
+                                Log.d("error", ex.getMessage());
+                            }
+                            progressBarHandler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressBarStatus);
+                                }
+                            });
+                        }
+                        // performing operation if file is downloaded,
+                        if (progressBarStatus >= 100) {
+                        // sleeping for 1 second after operation completed
+                        try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+                        // close the progress bar dialog
+                        progressBar.dismiss();
+                    }
+                }
+                }).start();
             } catch (Exception ex){
 
             }
         }
     }
+
+    private int importData() throws JSONException {
+        while (progressBarStatus <= 100) {
+            if (progressBarStatus < 20) {
+                progressBarHandler.post(new Runnable() {
+                    public void run() {
+                        progressBar.setMessage(getResources().getText(R.string.import_delete_data));
+                    }
+                });
+                APIClient.deleteData();
+                return 20;
+            } else if (progressBarStatus < 40) {
+                progressBarHandler.post(new Runnable() {
+                    public void run() {
+                        progressBar.setMessage(getResources().getText(R.string.import_driver));
+                    }
+                });
+                APIClient.getRiders();
+                return 40;
+            } else if (progressBarStatus < 60) {
+                progressBarHandler.post(new Runnable() {
+                    public void run() {
+                        progressBar.setMessage(getResources().getText(R.string.import_judgment));
+                    }
+                });
+                APIClient.getJudgments();
+                return 60;
+            } else if (progressBarStatus < 80) {
+                progressBarHandler.post(new Runnable() {
+                    public void run() {
+                        progressBar.setMessage(getResources().getText(R.string.import_reward));
+                    }
+                });
+                APIClient.getRewards();
+                return 80;
+            } else{
+                progressBarHandler.post(new Runnable() {
+                    public void run() {
+                        progressBar.setMessage(getResources().getText(R.string.import_stage));
+                    }
+                });
+                APIClient.getStages();
+                return 100;
+            }
+        }//end of while
+        return 100;
+    }//end of doOperation
 }
