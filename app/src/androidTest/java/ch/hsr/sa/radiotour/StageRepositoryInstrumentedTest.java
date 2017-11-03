9a7746e7c2b7449561e3909ef3db26fd887fdee0
Hawner <dominik_good@hotmail.com>
2017-11-03 12:02:52 +0100

    added plain Test Classes

---
+++ app/src/androidTest/java/ch/hsr/sa/radiotour/StageRepositoryInstrumentedTest.java
@@ -0,0 +1,10 @@
+package ch.hsr.sa.radiotour;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class StageRepositoryInstrumentedTest {
+
+}
