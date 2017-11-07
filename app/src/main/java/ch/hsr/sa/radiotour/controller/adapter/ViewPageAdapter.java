18a990fa830942551573705d9f4bf7b4b391d25b
Hawner <dominik_good@hotmail.com>
2017-11-07 15:25:32 +0100

    fixed re selection of race

--- app/src/main/java/ch/hsr/sa/radiotour/controller/adapter/ViewPageAdapter.java
+++ app/src/main/java/ch/hsr/sa/radiotour/controller/adapter/ViewPageAdapter.java
@@ -1,5 +1,6 @@
 package ch.hsr.sa.radiotour.controller.adapter;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -14,28 +15,28 @@ public class ViewPageAdapter extends FragmentStatePagerAdapter {
     private final List<Fragment> fragments = new ArrayList<>();
     private final List<String> titles = new ArrayList<>();
     private boolean detail = false;
+    private RiderRaceGroupFragment riderRaceGroupFragment;
+    private RaceFragment raceFragment;
 
     public ViewPageAdapter(FragmentManager fm) {
         super(fm);
+        riderRaceGroupFragment = new RiderRaceGroupFragment();
+        raceFragment = new RaceFragment();
     }
 
     public void setDetail(boolean in) {
         detail = in;
+        if(in){
+            fragments.set(0, riderRaceGroupFragment);
+        } else {
+            fragments.set(0, raceFragment);
+        }
+        notifyDataSetChanged();
     }
 
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
-            if (!detail) {
-                return new RaceFragment();
-            } else {
-                detail = false;
-                return new RiderRaceGroupFragment();
-            }
-        } else {
-            return fragments.get(position);
-        }
-
+        return fragments.get(position);
     }
 
     @Override
