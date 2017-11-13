8ccac3a1eb374832d5998c1ca9db1d6517f3f7e6
Urs Forrer <mail@ursforrer.ch>
2017-11-13 10:27:34 +0100

    Added the missing tests

---
+++ app/src/androidTest/java/ch/hsr/sa/radiotour/JudgmentRiderConnectionInstrumentedTest.java
@@ -0,0 +1,157 @@
+package ch.hsr.sa.radiotour;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.hsr.sa.radiotour.business.presenter.JudgmentPresenter;
+import ch.hsr.sa.radiotour.business.presenter.RiderPresenter;
+import ch.hsr.sa.radiotour.dataaccess.RadioTourApplication;
+import ch.hsr.sa.radiotour.dataaccess.interfaces.IJudgmentRepository;
+import ch.hsr.sa.radiotour.dataaccess.interfaces.IJudgmentRiderConnectionRepository;
+import ch.hsr.sa.radiotour.dataaccess.interfaces.IRiderRepository;
+import ch.hsr.sa.radiotour.dataaccess.models.Judgement;
+import ch.hsr.sa.radiotour.dataaccess.models.JudgmentRiderConnection;
+import ch.hsr.sa.radiotour.dataaccess.models.Rider;
+import ch.hsr.sa.radiotour.dataaccess.repositories.JudgmentRepository;
+import ch.hsr.sa.radiotour.dataaccess.repositories.JudgmentRiderConnectionRepository;
+import ch.hsr.sa.radiotour.dataaccess.repositories.RiderRepository;
+import io.realm.Realm;
+import io.realm.RealmList;
+import io.realm.RealmResults;
+
+@RunWith(AndroidJUnit4.class)
+public class JudgmentRiderConnectionInstrumentedTest {
+    private Realm realm;
+    private JudgmentRiderConnectionRepository judgmentRiderConnectionRepository;
+    private IJudgmentRiderConnectionRepository.OnSaveJudgmentRiderConnectionCallback onSaveJudgmentRiderConnectionCallback;
+    private RiderRepository riderRepository;
+    private IRiderRepository.OnSaveRiderCallback onSaveRiderCallback;
+    private IJudgmentRepository.OnSaveJudgmentCallback onSaveJudgmentCallback;
+    private JudgmentRepository judgmentRepository;
+
+    @Before
+    public void initTestData() {
+        this.judgmentRiderConnectionRepository = new JudgmentRiderConnectionRepository();
+        this.riderRepository = new RiderRepository();
+        this.judgmentRepository = new JudgmentRepository();
+        realm = Realm.getInstance(RadioTourApplication.getInstance());
+        initCallbacks();
+        realm.executeTransaction((Realm db) ->  {
+            db.where(JudgmentRiderConnection.class).findAll().deleteAllFromRealm();
+            db.where(Rider.class).findAll().deleteAllFromRealm();
+            db.where(Judgement.class).findAll().deleteAllFromRealm();
+        });
+    }
+
+    private void initCallbacks() {
+        onSaveJudgmentRiderConnectionCallback = new IJudgmentRiderConnectionRepository.OnSaveJudgmentRiderConnectionCallback() {
+            @Override
+            public void onSuccess() {
+                successFullyAdded();
+            }
+
+            @Override
+            public void onError(String message) {
+            }
+        };
+        onSaveRiderCallback = new IRiderRepository.OnSaveRiderCallback() {
+            @Override
+            public void onSuccess() {
+                successFullyAdded();
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+        };
+        onSaveJudgmentCallback = new IJudgmentRepository.OnSaveJudgmentCallback() {
+            @Override
+            public void onSuccess() {
+                successFullyAdded();
+            }
+
+            @Override
+            public void onError(String message) {
+            }
+        };
+    }
+
+    private void addRider() {
+        Rider rider = new Rider();
+        rider.setCountry("swiss");
+        rider.setName("testrider");
+        rider.setStartNr(15);
+        rider.setTeamName("Swiss");
+        rider.setTeamShortName("CH");
+        synchronized (this){
+            riderRepository.addRider(rider, onSaveRiderCallback);
+        }
+    }
+
+    private void addJudgment() {
+        Judgement judgement = new Judgement();
+        judgement.setDistance(100);
+        judgement.setName("judgment");
+        judgement.setRewardId(93);
+
+        synchronized (this){
+            judgmentRepository.addJudgment(judgement, onSaveJudgmentCallback);
+        }
+    }
+
+    private void successFullyAdded() {
+        notifyAll();
+    }
+
+    @Test
+    public void addRewardRiderConnection(){
+        addRider();
+        addJudgment();
+        RealmList<Rider> riderList = new RealmList<>();
+        RealmList<Judgement> judgementList = new RealmList<>();
+        riderList.add(RiderPresenter.getInstance().getRiderByStartNr(15));
+        judgementList.add(JudgmentPresenter.getInstance().getJudgmentsById(93).first());
+
+        JudgmentRiderConnection judgmentRiderConnection = new JudgmentRiderConnection();
+        judgmentRiderConnection.setRank(1);
+        judgmentRiderConnection.setJudgements(judgementList);
+        judgmentRiderConnection.setRider(riderList);
+
+        synchronized (this){
+            judgmentRiderConnectionRepository.addJudgmentRiderConnection(judgmentRiderConnection, onSaveJudgmentRiderConnectionCallback);
+        }
+
+        Assert.assertEquals(1, realm.where(JudgmentRiderConnection.class).findAll().first().getRank());
+    }
+
+    @Test
+    public void clearAllRewardRiderConnections(){
+        addRider();
+        addJudgment();
+        RealmList<Rider> riderList = new RealmList<>();
+        RealmList<Judgement> judgementList = new RealmList<>();
+        riderList.add(RiderPresenter.getInstance().getRiderByStartNr(15));
+        judgementList.add(JudgmentPresenter.getInstance().getJudgmentsById(93).first());
+
+        JudgmentRiderConnection judgmentRiderConnection = new JudgmentRiderConnection();
+        judgmentRiderConnection.setRank(1);
+        judgmentRiderConnection.setJudgements(judgementList);
+        judgmentRiderConnection.setRider(riderList);
+
+        synchronized (this){
+            judgmentRiderConnectionRepository.addJudgmentRiderConnection(judgmentRiderConnection, onSaveJudgmentRiderConnectionCallback);
+        }
+
+        synchronized (this){
+            judgmentRiderConnectionRepository.clearAllJudgmentRiderConnections();
+        }
+
+        Assert.assertEquals(0, realm.where(JudgmentRiderConnection.class).findAll().size());
+    }
+}
