90ac91bf786633cc908ca3e90a430435bee7c274
Hawner <dominik_good@hotmail.com>
2017-11-16 17:42:00 +0100

    fixed link

--- app/src/main/java/ch/hsr/sa/radiotour/controller/api/UrlLink.java
+++ app/src/main/java/ch/hsr/sa/radiotour/controller/api/UrlLink.java
@@ -9,7 +9,7 @@ public final class UrlLink {
     public static final String RIDERS = "api/masterdata/riders/stage/";
     public static final String STAGES = "public/stages/";
     public static final String JUDGEMENTS = "api/masterdata/judgements/";
-    public static final String STATES = "http://tlng.cnlab.ch/json_public/status.php";
+    public static final String STATES = "json_public/status.php";
     public static final String GLOBALSETTINGS = "api/getAllGlobalSettings";
     public static final String MAILLOTS = "api/masterdata/jerseys/race/";
 }
