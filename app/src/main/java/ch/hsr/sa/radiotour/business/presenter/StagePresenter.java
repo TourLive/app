a0d28d75e61a5cefd6dae609fad91e92fae28f52
Hawner <dominik_good@hotmail.com>
2017-11-17 12:28:36 +0100

    StageId Show on Init & on Import

--- app/src/main/java/ch/hsr/sa/radiotour/business/presenter/StagePresenter.java
+++ app/src/main/java/ch/hsr/sa/radiotour/business/presenter/StagePresenter.java
@@ -1,5 +1,6 @@
 package ch.hsr.sa.radiotour.business.presenter;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import ch.hsr.sa.radiotour.business.presenter.interfaces.IStagePresenter;
 import ch.hsr.sa.radiotour.dataaccess.interfaces.IStageRepository;
 import ch.hsr.sa.radiotour.dataaccess.models.Stage;
 import ch.hsr.sa.radiotour.dataaccess.repositories.StageRepository;
+import ch.hsr.sa.radiotour.presentation.activites.MainActivity;
 
 public class StagePresenter implements IStagePresenter {
     private ArrayList<Fragment> fragments = new ArrayList<>();
@@ -27,15 +29,15 @@ public class StagePresenter implements IStagePresenter {
         this.fragments.add(frag);
     }
 
-
     @Override
     public void subscribeCallbacks() {
         onSaveStageCallback = new IStageRepository.OnSaveStageCallback() {
             @Override
-            public void onSuccess() {
+            public void onSuccess(Stage stage) {
                 for(Fragment frag : fragments){
                     // call specifc update function for each fragment type
                 }
+                MainActivity.getInstance().updateStageId(stage.getName());
             }
 
             @Override
@@ -53,7 +55,12 @@ public class StagePresenter implements IStagePresenter {
 
     @Override
     public void addStage(Stage stage) {
-        stageRepository.addStage(stage, onSaveStageCallback);
+         stageRepository.addStage(stage, onSaveStageCallback);
+    }
+
+    @Override
+    public Stage getStage(){
+        return stageRepository.getStage();
     }
 
     @Override
