e3052b3cb75aee81c195860d0dd72ed469541319
Hawner <dominik_good@hotmail.com>
2017-11-17 14:19:31 +0100

    race time implemented

--- app/src/main/java/ch/hsr/sa/radiotour/presentation/activites/MainActivity.java
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/activites/MainActivity.java
@@ -13,15 +13,14 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
-import org.json.JSONObject;
-import org.w3c.dom.Text;
-
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,8 +34,7 @@ import ch.hsr.sa.radiotour.business.presenter.RiderPresenter;
 import ch.hsr.sa.radiotour.business.presenter.RiderStageConnectionPresenter;
 import ch.hsr.sa.radiotour.business.presenter.StagePresenter;
 import ch.hsr.sa.radiotour.controller.adapter.ViewPageAdapter;
-import ch.hsr.sa.radiotour.controller.api.APIClient;
-import ch.hsr.sa.radiotour.controller.api.UrlLink;
+import ch.hsr.sa.radiotour.dataaccess.models.Stage;
 import ch.hsr.sa.radiotour.presentation.fragments.ImportFragment;
 import ch.hsr.sa.radiotour.presentation.fragments.MaillotsFragment;
 import ch.hsr.sa.radiotour.presentation.fragments.RaceFragment;
@@ -49,9 +47,14 @@ public class MainActivity extends AppCompatActivity {
     private static MainActivity activity;
     public ViewPageAdapter viewPageAdapter;
     public ViewPager viewPager;
+
     private Timer timerForUpdate;
-    private TimerTask timerTask;
+    private TimerTask timerTaskForUpdate;
+    private Timer timerForRace;
+    private TimerTask timerTaskForRace;
+
     private Handler uiHandler;
+
     private TextView heightView;
     private TextView topFieldActualGapView;
     private TextView topFieldVirtualGapView;
@@ -61,11 +64,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView velocityView;
     private TextView raceKilometerView;
     private TextView raceTimeView;
-    private static int UPDATE_TIME = 5000;
-    private static int DELAY_TIME = 10000;
-    private static int MIN_DISTANCE_CHANGE = 0;
+    private TextView startStopView;
+
+    private Boolean RACE_IN_PROGRESS = false;
+    private Time RACE_TIME = new Time(0);
+    private float DISTANCE_IN_METER = 0;
+    private float WHOLE_DISTANCE_IN_KM = 0;
+
+    private Location actualLocation;
     private LocationManager locationManager;
     private LocationListener locationListener;
+
+    private static int UPDATE_TIME = 5000;
+    private static int DELAY_TIME = 10000;
+    private static int UPDATE_TIME_FOR_RACE = 1000;
+    private static int DELAY_ZERO = 0;
+    private static int MIN_DISTANCE_CHANGE = 0;
 
     public static MainActivity getInstance() {
         return activity;
@@ -120,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViewsAndHandlers() {
         uiHandler = new Handler();
+        timerForRace = new Timer();
 
         heightView = (TextView) findViewById(R.id.txtHeightValue);
         topFieldActualGapView = (TextView) findViewById(R.id.txtTopFieldActualGap);
@@ -130,46 +145,76 @@ public class MainActivity extends AppCompatActivity {
         velocityView = (TextView) findViewById(R.id.txtVelocityValue);
         raceKilometerView = (TextView) findViewById(R.id.txtRacekilometerValue);
         raceTimeView = (TextView) findViewById(R.id.txtRacetimeValue);
+        startStopView = (TextView) findViewById(R.id.btnStartStopRace);
 
         if(StagePresenter.getInstance().getStage() != null)
-            updateStageId(StagePresenter.getInstance().getStage().getName());
+            updateStageId(StagePresenter.getInstance().getStage());
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            locationManager.getProvider(LocationManager.GPS_PROVIDER).supportsAltitude();
-            locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    uiHandler.post(() -> {
-                        heightView.setText(location.getAltitude() + "m");
-                    });
-                }
 
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-                    // Has to be implemented, but not needed
-                }
+        startStopView.setOnClickListener((click) -> {
+            if(!RACE_IN_PROGRESS){
+                RACE_IN_PROGRESS = true;
+                startStopView.setBackgroundColor(getColor(R.color.colorOlive));
+                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.getProvider(LocationManager.GPS_PROVIDER).supportsAltitude();
+                    locationListener = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            if(actualLocation != null) { DISTANCE_IN_METER += actualLocation.distanceTo(location); }
+                            actualLocation = location;
+                            uiHandler.post(() -> {
+                                heightView.setText(getString(R.string.header_prefix_m, actualLocation.getAltitude()));
+                                raceKilometerView.setText(getString(R.string.header_prefix_km, DISTANCE_IN_METER / 1000f, WHOLE_DISTANCE_IN_KM));
+                            });
+                        }
 
-                @Override
-                public void onProviderEnabled(String provider) {
-                   // Has to be implemented, but not needed
-                }
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+                            // Has to be implemented, but not needed
+                        }
 
-                @Override
-                public void onProviderDisabled(String provider) {
-                    // Has to be implemented, but not needed
-                }
-            };
+                        @Override
+                        public void onProviderEnabled(String provider) {
+                            // Has to be implemented, but not needed
+                        }
 
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_TIME, MIN_DISTANCE_CHANGE, locationListener);
-        }
+                        @Override
+                        public void onProviderDisabled(String provider) {
+                            // Has to be implemented, but not needed
+                        }
+                    };
+
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_TIME, MIN_DISTANCE_CHANGE, locationListener);
+
+                    timerForRace = new Timer();
+                    timerTaskForRace = new TimerTask() {
+                        @Override
+                        public void run() {
+                            RACE_TIME.setTime(RACE_TIME.getTime() + UPDATE_TIME_FOR_RACE);
+                            uiHandler.post(() -> {
+                                raceTimeView.setText(convertLongToTimeString(RACE_TIME.getTime()));
+                            });
+                        }
+                    };
+                    timerForRace.schedule(timerTaskForRace, DELAY_ZERO, UPDATE_TIME_FOR_RACE);
+
+                }
+            } else{
+                RACE_IN_PROGRESS = false;
+                locationManager.removeUpdates(locationListener);
+                actualLocation = null;
+                startStopView.setBackgroundColor(getColor(R.color.colorPrimaryUltraLight));
+                timerForRace.cancel();
+            }
+        });
 
 
         timerForUpdate = new Timer();
-        timerTask = new TimerTask() {
+        timerTaskForUpdate = new TimerTask() {
             @Override
             public void run() {
                 try {
@@ -179,7 +224,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-        timerForUpdate.schedule(timerTask, DELAY_TIME, UPDATE_TIME);
+        timerForUpdate.schedule(timerTaskForUpdate, DELAY_TIME, UPDATE_TIME);
     }
 
     private void closeDetailJudgmentFragment() {
@@ -227,19 +272,22 @@ public class MainActivity extends AppCompatActivity {
         });
         update.start();
         update.join();
-
     }
-
-    /*public void getHeight() {
-
-    }*/
-
-    public void updateStageId(String name){
+    
+    public void updateStageId(Stage stage){
         Pattern p = Pattern.compile("(\\d+)");
-        Matcher m = p.matcher(name);
+        Matcher m = p.matcher(stage.getName());
         m.find();
+        WHOLE_DISTANCE_IN_KM = stage.getDistance();
         uiHandler.post(() -> {
             stageView.setText(m.group(0));
+            raceKilometerView.setText(getString(R.string.header_prefix_km, 0.0, WHOLE_DISTANCE_IN_KM));
         });
+    }
+
+    private String convertLongToTimeString(long time) {
+        Date date = new Date(time);
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        return formatter.format(date);
     }
 }
