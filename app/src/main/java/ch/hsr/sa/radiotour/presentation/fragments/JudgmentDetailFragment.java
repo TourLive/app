e400ca01595452757f8da5b2d7d030781a65b758
Urs Forrer <mail@ursforrer.ch>
2017-11-21 15:10:01 +0100

    Bugfixing
    Bugfixing

--- app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/JudgmentDetailFragment.java
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/JudgmentDetailFragment.java
@@ -3,6 +3,7 @@ package ch.hsr.sa.radiotour.presentation.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -61,7 +62,7 @@ public class JudgmentDetailFragment extends Fragment implements View.OnClickList
         rvRidersToSelect = (RecyclerView) root.findViewById(R.id.rvRidersToSelect);
         riderBasicAdapter = new RiderBasicAdapter(RiderPresenter.getInstance().getAllRidersReturned());
         rvRidersToSelect.setAdapter(riderBasicAdapter);
-        rvRidersToSelect.setLayoutManager(new GridLayoutManager(getContext(), 12));
+        rvRidersToSelect.setLayoutManager(new GridLayoutManager(this.getContext(), 8, LinearLayoutManager.HORIZONTAL, false));
         rvRidersToSelect.setHasFixedSize(true);
         initRankView(root);
 
