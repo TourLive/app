ec767544f7cf956d6876571b87ca2d01c17d7220
Urs Forrer <mail@ursforrer.ch>
2017-11-27 09:55:39 +0100

    Implemented SplashActivity with Full Background Image
    Implemented SplashActivity with Full Background Image

---
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/activites/SplashActivity.java
@@ -0,0 +1,20 @@
+package ch.hsr.sa.radiotour.presentation.activites;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Urs Forrer on 27.11.2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        //startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        // close splash activity
+        //finish();
+    }
+}
