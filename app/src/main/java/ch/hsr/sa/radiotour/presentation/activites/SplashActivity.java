a851d850c11c9252d4be78162ccfe7026d5c2272
Urs Forrer <mail@ursforrer.ch>
2017-11-27 10:00:11 +0100

    Wait x seconds on the start up

--- app/src/main/java/ch/hsr/sa/radiotour/presentation/activites/SplashActivity.java
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/activites/SplashActivity.java
@@ -2,19 +2,19 @@ package ch.hsr.sa.radiotour.presentation.activites;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Urs Forrer on 27.11.2017.
- */
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Start home activity
-        //startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        // close splash activity
-        //finish();
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 2000);
+
     }
 }
