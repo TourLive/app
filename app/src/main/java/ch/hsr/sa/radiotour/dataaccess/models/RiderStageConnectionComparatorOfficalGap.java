726ade713a430994d623653263008509e69fc098
Hawner <dominik_good@hotmail.com>
2018-02-22 09:37:28 +0100

    Added Comparators for other categories & Import calculations implemented

--- app/src/main/java/ch/hsr/sa/radiotour/dataaccess/models/RiderStageConnectionComparatorOfficalGap.java
+++ app/src/main/java/ch/hsr/sa/radiotour/dataaccess/models/RiderStageConnectionComparatorOfficalGap.java
@@ -3,7 +3,7 @@ package ch.hsr.sa.radiotour.dataaccess.models;
 import java.util.Comparator;
 
 
-public class RiderStageConnectionComparator implements Comparator<RiderStageConnection> {
+public class RiderStageConnectionComparatorOfficalGap implements Comparator<RiderStageConnection> {
     @Override
     public int compare(RiderStageConnection r1, RiderStageConnection r2) {
         return Long.compare(r1.getOfficialGap(), r2.getOfficialGap());
