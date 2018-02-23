374e9078901b52a59c31f606cfbc57ddf98faa06
Hawner <dgood@hsr.ch>
2018-02-23 14:10:57 +0100

    Added update of "Wertung" values

--- app/src/main/java/ch/hsr/sa/radiotour/dataaccess/models/RiderStageConnectionComparatorMountainPoints.java
+++ app/src/main/java/ch/hsr/sa/radiotour/dataaccess/models/RiderStageConnectionComparatorMountainPoints.java
@@ -6,6 +6,6 @@ import java.util.Comparator;
 public class RiderStageConnectionComparatorMountainPoints implements Comparator<RiderStageConnection> {
     @Override
     public int compare(RiderStageConnection r1, RiderStageConnection r2) {
-        return Long.compare(r1.getMountainBonusPoints(), r2.getMountainBonusPoints());
+        return Long.compare(r2.getMountainBonusPoints(), r1.getMountainBonusPoints());
     }
 }
