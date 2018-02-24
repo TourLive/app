d56d957313030ac2289d14ef46b0758bd96a829b
Urs Forrer <mail@ursforrer.ch>
2018-02-24 11:08:20 +0100

    ButterKnfie on Maillots
    ButterKnfie on Maillots

--- app/src/main/java/ch/hsr/sa/radiotour/presentation/models/ButterKnifeViewHolderList.java
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/models/ButterKnifeViewHolderList.java
@@ -1,6 +1,5 @@
 package ch.hsr.sa.radiotour.presentation.models;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import net.gotev.recycleradapter.RecyclerAdapterNotifier;
@@ -8,9 +7,13 @@ import net.gotev.recycleradapter.RecyclerAdapterViewHolder;
 
 import butterknife.ButterKnife;
 
-public abstract class ButterKnifeViewHolder extends RecyclerView.ViewHolder  {
-    public ButterKnifeViewHolder(View itemView) {
-        super(itemView);
+/**
+ * Created by uforrer on 23.02.2018.
+ */
+
+public abstract class ButterKnifeViewHolderList extends RecyclerAdapterViewHolder {
+    public ButterKnifeViewHolderList(View itemView, RecyclerAdapterNotifier adapter) {
+        super(itemView, adapter);
         ButterKnife.bind(this, itemView);
     }
 }
