95093fd9d69f6e8428336b9a42d38ec7901ac3b5
Hawner <dominik_good@hotmail.com>
2018-04-05 15:19:40 +0200

    fixed url lnks to new api routes

--- app/src/main/java/ch/hsr/sa/radiotour/controller/api/UrlLink.java
+++ app/src/main/java/ch/hsr/sa/radiotour/controller/api/UrlLink.java
@@ -2,13 +2,13 @@ package ch.hsr.sa.radiotour.controller.api;
 
 
 public final class UrlLink {
-    public static final String RIDERS = "/riderStageConnection";
-    public static final String STAGES = "stage/";
-    public static final String RACE  = "race/";
-    public static final String JUDGEMENTS = "api/masterdata/judgements/";
+    public static final String RIDERS = "riderStageConnections/stages/";
+    public static final String STAGES = "stages/";
+    public static final String RACE  = "races/";
+    public static final String JUDGEMENTS = "judgments/stages/";
     public static final String STATES = "json_public/status.php";
     public static final String GLOBALSETTINGS = "settings";
-    public static final String MAILLOTS = "/maillot/stage/";
+    public static final String MAILLOTS = "/maillots/stages/";
     public static final String RIDERJERSEY = "json_public/riderjerseystartstage.php?stage=";
     public static final String STATUSPAGE = "/status";
     private UrlLink() {
