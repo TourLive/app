329e7001c998d1fa64e1ebf2452d2af0d2a75ed7
Hawner <dgood@hsr.ch>
2018-04-06 08:47:57 +0200

    import fix for rider unkown, seperate call for status

--- app/src/main/java/ch/hsr/sa/radiotour/controller/api/UrlLink.java
+++ app/src/main/java/ch/hsr/sa/radiotour/controller/api/UrlLink.java
@@ -2,15 +2,14 @@ package ch.hsr.sa.radiotour.controller.api;
 
 
 public final class UrlLink {
-    public static final String RIDERS = "riderStageConnections/stages/";
+    public static final String RIDERS = "riderstageconnections/stages/";
     public static final String STAGES = "stages/";
     public static final String RACE  = "races/";
     public static final String JUDGEMENTS = "judgments/stages/";
     public static final String REWARDS = "rewards";
-    public static final String STATES = "json_public/status.php";
+    public static final String STATES = "http://tlng.cnlab.ch/json_public/status.php";
     public static final String GLOBALSETTINGS = "settings";
     public static final String MAILLOTS = "/maillots/stages/";
-    public static final String RIDERJERSEY = "json_public/riderjerseystartstage.php?stage=";
     public static final String STATUSPAGE = "/status";
     private UrlLink() {
         throw new IllegalStateException("Static class");
