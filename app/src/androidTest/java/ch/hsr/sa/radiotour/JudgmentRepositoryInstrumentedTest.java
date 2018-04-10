c9d6979c8754f086a635542c3a179470975805a4
Urs Forrer <mail@ursforrer.ch>
2018-04-10 15:56:48 +0200

    Refactored some InstrumentedTests
    Refactored some InstrumentedTests

--- app/src/androidTest/java/ch/hsr/sa/radiotour/JudgmentRepositoryInstrumentedTest.java
+++ app/src/androidTest/java/ch/hsr/sa/radiotour/JudgmentRepositoryInstrumentedTest.java
@@ -8,6 +8,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Random;
+
 import ch.hsr.sa.radiotour.dataaccess.RadioTourApplication;
 import ch.hsr.sa.radiotour.dataaccess.interfaces.IJudgmentRepository;
 import ch.hsr.sa.radiotour.dataaccess.interfaces.IRewardRepository;
@@ -112,14 +114,20 @@ public class JudgmentRepositoryInstrumentedTest {
     @Test
     public void clearAllJudgments(){
         Judgement judgement = new Judgement();
+        judgement.setId(new Random().nextLong());
         judgement.setDistance(100);
         judgement.setName("judgment");
         judgement.setRewardId(93);
 
+        Judgement judgementTwo = new Judgement();
+        judgementTwo.setId(new Random().nextLong());
+        judgementTwo.setDistance(100);
+        judgementTwo.setName("judgment 2");
+        judgementTwo.setRewardId(93);
+
         synchronized (this){
             judgmentRepository.addJudgment(judgement, onSaveJudgmentCallback);
-            judgement.setName("judgment2");
-            judgmentRepository.addJudgment(judgement, onSaveJudgmentCallback);
+            judgmentRepository.addJudgment(judgementTwo, onSaveJudgmentCallback);
         }
 
         synchronized (this){
