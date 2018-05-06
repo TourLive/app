5089cb94630ca0b9f2d2b0b78fda915f2b156849
Urs Forrer <mail@ursforrer.ch>
2018-05-06 09:40:09 +0200

    Done things with middle priority
    Done things with middle priority

--- app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/RaceFragment.java
+++ app/src/main/java/ch/hsr/sa/radiotour/presentation/fragments/RaceFragment.java
@@ -3,6 +3,8 @@ package ch.hsr.sa.radiotour.presentation.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
